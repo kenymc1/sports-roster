071a7ab81033b65a4cf56d7636f8c9eaae20785d
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-05-09 14:08:33 -0500

    setup

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Sports!!!</h2>
+        <button className="btn btn-info">Lets Play</button>
+      </div>
+    );
+  }
+}
+
+export default App;
